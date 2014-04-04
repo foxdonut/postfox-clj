31896b02481a70f88a68d44a637ac6d8e9d6e289
foxdonut <fdaoud@proinbox.com>
2014-04-03 22:31:18 -0400

    Working towards a Clojure implementation.

--- src/postfox/core.clj
+++ src/postfox/core.clj
@@ -1,15 +1,54 @@
-(ns postfox.core)
+(ns postfox.core
+  (:require
+    [clojure.test :refer [function?]]))
 
 (defn arg-count [func]
-  (->> (class func)
-    (.getDeclaredMethods)
-    (filter #(= "invoke" (.getName %)))
-    first
-    (.getParameterTypes)
-    alength))
+  (or
+    (:arg-count (meta func))
+    (->> (class func)
+      (.getDeclaredMethods)
+      (filter #(= "invoke" (.getName %)))
+      (map #(alength (.getParameterTypes %)))
+      (apply max))))
+
+(defn arity [arg-count func]
+  (with-meta func {:arg-count arg-count}))
+
+(defn apply-fn [func arg-count lst]
+  (cons (apply func (-> (take arg-count lst) reverse)) (drop arg-count lst)))
 
 (defn qtn [& elems]
-  nil)
+  (->
+    (reduce
+      (fn [state next]
+        (let [args (:args state)]
+          (if (function? next)
+            (let [
+                func (:func state)
+                nextFunc (apply partial (cons next args))]
+              {:func
+                 (if (function? func)
+                   (comp nextFunc func)
+                   nextFunc)
+               :args []})
+            (assoc state :args (conj args next)))))
+      {:args []}
+      elems)
+    (with-meta {:qtn true})))
+
+(defn qtn? [func]
+  (-> (:qtn (meta func)) true?))
 
 (defn postfox [& elems]
-  nil)
+  (->
+    (reduce
+      (fn [lst next]
+        (if (function? next)
+          (if (qtn? next)
+            (cons next lst)
+            (apply-fn next (arg-count next) lst))
+          (cons next lst)))
+      []
+      elems)
+    first))
+
--- test/postfox/core_test.clj
+++ test/postfox/core_test.clj
@@ -14,15 +14,19 @@
   (testing "postfox"
     (testing "function length"
       (is (= 14 (postfox 5 1 2 + 4 * + 3 -))))
+
     (testing "quotations and function arity"
-      (is (= 15 (postfox [1 2 3 4 5] (qtn +) 0 reduce))))
+      (is (= 15 (postfox (qtn +) 0 [1 2 3 4 5] reduce))))
+
     (testing "partial function application in arity"
-      (is (= [10 20 30 40 50] (postfox [1 2 3 4 5] (qtn 10 *) map))))
+      (is (= [10 20 30 40 50] (postfox (qtn 10 *) [1 2 3 4 5] (arity 2 map)))))
+
     (testing "multiple partial function application in quotation"
-      (is (= [16 20 24 28 32] (postfox [1 2 3 4 5] (qtn 3 + 4 *) map))))
+      (is (= [16 20 24 28 32] (postfox (qtn 3 + 4 *) [1 2 3 4 5] (arity 2 map)))))
+
     (testing "utility functions"
       (is (= 42 (postfox 84 2 /)))
       (is (= 5 (postfox "hello" count)))
       (is (= true (postfox 8 (+ 4 4) =)))
-      (is (= [2 4 42] 2 [4 42] cons)))))
+      (is (= [2 4 42] (postfox 2 [4 42] cons))))))
 
